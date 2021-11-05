4e44bd8c5a88bdc28d8baddcf4d842095b5275d7
SanskritiKushik5 <sanskritikushik23@gmail.com>
2021-11-05 15:26:10 +0530

    Basic setup

---
+++ android/app/src/main/kotlin/sk5/com/world_time_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package sk5.com.world_time_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/loading.dart
@@ -0,0 +1,19 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({Key? key}) : super(key: key);
+
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Loading Screen'),
+    );
+  }
+}
