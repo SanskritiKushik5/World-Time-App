e4f314c90e8389d841431cf17491a771e1c13b18
SanskritiKushik5 <sanskritikushik23@gmail.com>
2021-11-06 17:45:11 +0530

    Testing and Logic for API

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -11,16 +11,24 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
-    Response response = await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+  void getTime() async {
+    Response response = await get(Uri.parse('https://worldtimeapi.org/api/timezone/Europe/London'));
     Map data = jsonDecode(response.body);
-    print(data["title"]);
+
+    // Properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+
+    // DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
   @override
   Widget build(BuildContext context) {
