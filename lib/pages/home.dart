6812d9cb1d96f661e1d2f7d1369664286a356509
SanskritiKushik5 <sanskritikushik23@gmail.com>
2021-11-06 22:01:01 +0530

    Formatting and Showing Date

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,25 +9,49 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  // Map data = {};
+  Map data = {};
 
   @override
   Widget build(BuildContext context) {
 
-    final data = ModalRoute.of(context)!.settings.arguments;
-    print(data);
+    data = ModalRoute.of(context)!.settings.arguments as Map;
 
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          FlatButton.icon(
-            onPressed: () {
-              Navigator.pushNamed(context, '/location');
-            },
-            icon: Icon(Icons.edit_location),
-            label: Text('Edit Location'),
-          )
-        ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+          child: Column(
+            children: <Widget>[
+              FlatButton.icon(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/location');
+                },
+                icon: const Icon(Icons.edit_location),
+                label: const Text('Edit Location'),
+              ),
+              const SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    data['location'],
+                    style: const TextStyle(
+                      fontSize: 28.0,
+                      letterSpacing: 2.0
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 20.0),
+              Text(
+                data['time'],
+                style: const TextStyle(
+                  fontSize: 66.0,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime {
   late String location;
@@ -23,7 +24,7 @@ class WorldTime {
       DateTime now = DateTime.parse(datetime);
       now = now.add(Duration(hours: int.parse(offset)));
 
-      time = now.toString();
+      time = DateFormat.jm().format(now);
     }catch (e) {
       print('caught error: $e');
       time = 'Could not get time data';
